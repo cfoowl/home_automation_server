a2b13fbfd70040b78cb02904a2a09120c22337f4
cfoowl <alicevancypher@gmail.com>
2024-11-29 10:48:56 +0100

    [Cfoowl][FEAT] - Add Device Management Endpoints
    Implemented additional CRUD endpoints for device management in the API. New endpoints allow retrieval of all devices, fetching a specific device by ID, and deletion of a device. These enhancements ensure better handling and manipulation of device data within the system.

--- app/api/endpoints/device_endpoints.py
+++ app/api/endpoints/device_endpoints.py
@@ -1,9 +1,21 @@
 from sqlalchemy.orm import Session
 from app.models.device import Device
 
-def create_device(db: Session, name: str, type: str, metadata: dict = {}):
+def create_device_endpoint(db: Session, name: str, type: str, metadata: dict = {}):
     new_device = Device(name=name, type=type, device_metadata=metadata)
     db.add(new_device)
     db.commit()
     db.refresh(new_device)
     return {"id": new_device.id, "name": new_device.name}
+
+def get_all_devices_endpoint(db: Session):
+    return db.query(Device).all()
+
+def get_device_by_id_endpoint(db: Session, device_id: int):
+    return db.query(Device).filter(Device.id == device_id).first()
+
+def delete_device_by_id_endpoint(db: Session, device_id: int):
+    device = db.query(Device).filter(Device.id == device_id).first()
+    db.delete(device)
+    db.commit()
+    return {"message": "Device deleted successfully"}
--- app/api/router.py
+++ app/api/router.py
@@ -1,10 +1,22 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from app.core.dependencies import get_db
-from app.api.endpoints.device_endpoints import create_device as create_device_endpoint
+from app.api.endpoints.device_endpoints import *
 
 router = APIRouter()
 
 @router.post("/devices/")
 def create_device(name: str, type: str, metadata: dict = {}, db: Session = Depends(get_db)):
     return create_device_endpoint(db=db, name=name, type=type, metadata=metadata)
+
+@router.get("/devices/")
+def get_all_devices(db: Session = Depends(get_db)):
+    return get_all_devices_endpoint(db=db)
+
+@router.get("/devices/{device_id}")
+def get_device_by_id(device_id: int, db: Session = Depends(get_db)):
+    return get_device_by_id_endpoint(db=db, device_id=device_id)
+
+@router.delete("/devices/{device_id}")
+def delete_device_by_id(device_id: int, db: Session = Depends(get_db)):
+    return delete_device_by_id_endpoint(db=db, device_id=device_id)
